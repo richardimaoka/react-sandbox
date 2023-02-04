cb8c6147b05c498cd177db2126b73979fbdfc43f
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-02-04 18:34:50 +0900

    dynamic ozaki

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 
 interface GreetingProps {
   name: string;
 }
 
 const Greeting = memo(function Greeting({ name }: GreetingProps) {
+  console.log("rendering greeting");
   return <h1>Hello, {name}!</h1>;
 });
 
 function App() {
+  const [generation, setGeneration] = useState(1);
+  const nextGen = () => {
+    setGeneration(generation + 1);
+  };
+  const ozakiName =
+    generation === 1 ? "ozaki zakio" : "ozaki zakio " + generation;
   return (
     <div>
-      <Greeting name="ozaki zakio" />
+      <Greeting name={ozakiName} />
+      <button type="button" onClick={nextGen}>
+        born
+      </button>
     </div>
   );
 }
